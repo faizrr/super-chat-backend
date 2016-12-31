2d001ea8c1982ca2a88bc0b1072a8ecff660caab
Roman <zummo3@gmail.com>
2016-12-31 16:25:04 +0300

    Use metaclass

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,29 +4,31 @@ class User < ApplicationRecord
   has_many :user_chat_rooms
   has_many :chat_rooms, through: :user_chat_rooms
 
-  def self.from_token_payload(payload)
-    @jwt_payload = payload
+  class << self
+    def from_token_payload(payload)
+      @jwt_payload = payload
 
-    raise ActiveRecord::RecordNotFound unless payload['sub']
+      raise ActiveRecord::RecordNotFound unless payload['sub']
 
-    where(auth0_id: payload['sub']).first_or_initialize do |user|
-      profile = get_profile_info
+      where(auth0_id: payload['sub']).first_or_initialize do |user|
+        profile = get_profile_info
 
-      user.auth0_id = profile['user_id']
-      user.name = profile['name']
+        user.auth0_id = profile['user_id']
+        user.name = profile['name']
 
-      user.save!
+        user.save!
+      end
     end
-  end
 
-  private
+    private
 
-  def self.get_profile_info
-    jwt = Knock::AuthToken.new(payload: @jwt_payload).token
+    def get_profile_info
+      jwt = Knock::AuthToken.new(payload: @jwt_payload).token
 
-    url = Rails.application.secrets.auth0_url + '/tokeninfo'
-    res = RestClient.post(url, { id_token: jwt }.to_json, headers = { 'content-type': 'application/json' })
+      url = Rails.application.secrets.auth0_url + '/tokeninfo'
+      res = RestClient.post(url, { id_token: jwt }.to_json, headers = { 'content-type': 'application/json' })
 
-    JSON.parse(res.body)
+      JSON.parse(res.body)
+    end
   end
 end
